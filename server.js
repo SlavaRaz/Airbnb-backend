4235e4c745729ba651072d130e48e41eb0f34b83
SlavaRaz <slavarazaranov87@gmail.com>
2025-01-29 16:12:38 +0200

    change car to stay

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser'
 import { authRoutes } from './api/auth/auth.routes.js'
 import { userRoutes } from './api/user/user.routes.js'
 import { reviewRoutes } from './api/review/review.routes.js'
-import { carRoutes } from './api/car/car.routes.js'
+import { stayRoutes } from './api/stay/stay.routes.js'
 import { setupSocketAPI } from './services/socket.service.js'
 
 import { setupAsyncLocalStorage } from './middlewares/setupAls.middleware.js'
@@ -37,7 +37,7 @@ app.all('*', setupAsyncLocalStorage)
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
 app.use('/api/review', reviewRoutes)
-app.use('/api/car', carRoutes)
+app.use('/api/stay', stayRoutes)
 
 setupSocketAPI(server)
 
