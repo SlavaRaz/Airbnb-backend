14fd0ad07e428049d74b37239b838b41a2a4c57e
SlavaRaz <slavarazaranov87@gmail.com>
2025-02-09 23:56:11 +0200

    update public with carousel css

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-  dbURL: process.env.MONGO_URL ,
-  dbName : process.env.DB_NAME 
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://team:1234@cluster0.bn4wz.mongodb.net/',
+  dbName : process.env.DB_NAME || 'stay_db'
 }
